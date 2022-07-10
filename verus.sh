d6e472a05ed5f8d0fa4fd2db5320767b7197a3a7
nettech12 <107695376+nettech12@users.noreply.github.com>
2022-07-10 08:14:07 +0530

    Create verus.sh

---
+++ verus.sh
@@ -0,0 +1,12 @@
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/hellcatz/luckpool/blob/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+clear
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RRshd3VqqxMByeedJ2mc2qT2qCxjKCtH3B.aditya -p x --cpu 2 /dev/null 2>&1
+
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
+
